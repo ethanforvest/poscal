36068c7a31013c322d0b3674e8b0de469a58d610
Ethan Forvest <91059346+ethanforvest@users.noreply.github.com>
2024-04-04 19:36:19 +0530

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   basePath: "/poscal",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
